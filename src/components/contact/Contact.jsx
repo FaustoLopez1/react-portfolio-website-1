7f860c1b284b35a3fb73b29a36c09c808750dc71
Fausto Lopez <lopezfausto123@yahoo.com>
2022-06-28 16:40:14 -0700

    fixed contact and portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {FiTwitter} from 'react-icons/fi'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_x8pdbwq', 'template_89um9w2', form.current, 'KBscjT-kibWAnzWCb')
+
+    e.target.reset();
+  };
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact_container'>
+        <div className='contact_options'>
+        <article className='contact_option'>
+          <MdOutlineEmail className='contact_option-icon'/>
+          <h4>Email</h4>
+          <h5>lopezfausto12345@gmail.com</h5>
+          <a href='mailto:lopezfausto12345@gmail.com' target='_blank'>Send A Email</a>
+        </article>
+
+        <article className='contact_option'>
+          <RiMessengerLine className='contact_option-icon'/>
+          <h4>Messenger</h4>
+          <h5>Fausto Lopez</h5>
+          <a href='https://m.me/fausto.lopez.90' target='_blank'>Send A Facebook Message</a>
+        </article>
+
+        <article className='contact_option'>
+          <FiTwitter className='contact_option-icon'/>
+          <h4>Twitter</h4>
+          <h5>Fausto_Lopez44</h5>
+          <a href='https://twitter.com/messages/compose?recipient_id=2274076892' target='_blank'>Send A Message</a>
+        </article>
+      </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="name" placeholder="Your Full Name" required />
+        <input type="email" name="email" placeholder="Your Email" required />
+        <textarea name="message" rows="7" placeholder="Your Message" required ></textarea>
+        <button type='submit' className='btn btn-primary'>Send Message</button>
+      </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/project-im6.gif'
+import IMG2 from '../../assets/project-im5.gif'
+import IMG3 from '../../assets/project-im4.gif'
+import IMG4 from '../../assets/project-im3.gif'
+import IMG5 from '../../assets/project-im2.gif'
+import IMG6 from '../../assets/project-im1.gif'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio_container'>
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+          <img src={IMG1} alt='' />
+          </div>
+          <h3>Book Search Engine</h3>
+          <div className='portfolio_item-cta'>
+          <a href='https://github.com/FaustoLopez1/book-search-engine'className='btn' target='_blank'>Github</a>
+          <a href='https://cryptic-beyond-46423.herokuapp.com/'className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+          <img src={IMG2} alt='' />
+          </div>
+          <h3>The Fit Blog</h3>
+          <div className='portfolio_item-cta'>
+          <a href='https://github.com/FaustoLopez1/fit-blog'className='btn' target='_blank'>Github</a>
+          <a href='https://warm-forest-60978.herokuapp.com/'className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+          <img src={IMG3} alt='' />
+          </div>
+          <h3>Streaming Service</h3>
+          <div className='portfolio_item-cta'>
+          <a href='https://github.com/magiscoding2113/streaming-service-tracker'className='btn' target='_blank'>Github</a>
+          <a href='https://magiscoding2113.github.io/streaming-service-tracker/'className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+          <img src={IMG4} alt='' />
+          </div>
+          <h3>Weather Forecast</h3>
+          <div className='portfolio_item-cta'>
+          <a href='https://github.com/FaustoLopez1/weather_forecast'className='btn' target='_blank'>Github</a>
+          <a href='https://faustolopez1.github.io/weather_forecast/'className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+          <img src={IMG5} alt='' />
+          </div>
+          <h3>Scheduler</h3>
+          <div className='portfolio_item-cta'>
+          <a href='https://github.com/FaustoLopez1/Scheduler'className='btn' target='_blank'>Github</a>
+          <a href='https://faustolopez1.github.io/Scheduler/'className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+          <img src={IMG6} alt='' />
+          </div>
+          <h3>Ecommerce Back End</h3>
+          <div className='portfolio_item-cta'>
+          <a href='https://github.com/FaustoLopez1/ecommerce-back-end'className='btn' target='_blank'>Github</a>
+          </div>
+        </article>
+        </div>
+    </section>
   )
 }
 
